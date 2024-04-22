a7421ef3be089be5f6c938e452d02cac4dc7d533
lucky_lucky_cool <gonsor.y2@gmail.com>
2024-04-22 22:20:43 +0300

    added group identification

--- PredictionService.py
+++ PredictionService.py
@@ -1,11 +1,11 @@
-import numpy as np
-from matplotlib import pyplot as plt
 import os
+
 import librosa
-from scipy import signal
-import keras
+import numpy as np
 import tensorflow as tf
 from keras.applications.resnet import preprocess_input
+from matplotlib import pyplot as plt
+from scipy import signal
 
 import Model
 
@@ -18,7 +18,7 @@ def preprocess(images, labels):
     return preprocess_input(images), labels
 
 
-class Prediction:
+class PredictionService:
     model = None
 
     def __init__(self, model=None):
@@ -28,14 +28,14 @@ class Prediction:
             else:
                 self.model = model
 
-    def coverting_to_spectrogram(self, path, high_freq):
+    def coverting_to_spectrogram(self, absolute_path, high_freq):
         plt.rcParams["figure.figsize"] = [7.50, 3.50]
         plt.rcParams["figure.autolayout"] = True
         fig, ax = plt.subplots()
         hl = 512  # number of samples per time-step in spectrogram
         hi = 256  # Height of image
         wi = 256  # Width of image
-        y, sr = librosa.load(path)
+        y, sr = librosa.load(absolute_path)
 
         def get_high_freq(y, sr):
             b, a = signal.butter(10, 2000 / (sr / 2), btype='highpass')
@@ -52,17 +52,17 @@ class Prediction:
 
         if not os.path.exists('/to_predict'):
             os.mkdir('/to_predict')
-
+        path = os.path.basename(absolute_path)
         plt.axis('off')
         plt.savefig(f'/to_predict/{path}.png', bbox_inches='tight', pad_inches=0)
         plt.close()
 
-        os.remove(path)
+        # os.remove(path)
 
     def predict_audio(self, path, preprocess=False):
 
         self.coverting_to_spectrogram(path, high_freq=True)
-        image = tf.keras.utils.load_img(path=f'/to_predict/{path}.png', target_size=(256, 256),
+        image = tf.keras.utils.load_img(path=f'/to_predict/{os.path.basename(path)}.png', target_size=(256, 256),
                                         interpolation='bilinear')
         input_arr = tf.keras.utils.img_to_array(image)
         input_arr = np.array([input_arr])
@@ -71,5 +71,22 @@ class Prediction:
             input_arr = preprocess_input(input_arr)
 
         y_pred = self.model.predict(input_arr)
-        y_pred = int(np.round(y_pred[0][0]))
-        return y_pred
+        # y_pred = int(np.round(y_pred[0][0]))
+        return y_pred[0][0]
+
+    def predict_territory(self, directory_path):
+        file_list = os.listdir(directory_path)
+        skipped_files = []
+        true_count = 0
+        print(f'Test by {len(file_list)} samples')
+        for filename in file_list:
+            if '.wav' not in filename:
+                skipped_files.append(filename)
+                continue
+            prob_temp = self.predict_audio(directory_path + filename)
+            if int(np.round(prob_temp)) == 1:
+                true_count += 1
+            print(f'Probability of sample {filename} - {prob_temp}')
+        print(f'Indentified by {len(file_list) - len(skipped_files)} samples')
+        print(f'The bird was detected by {true_count} microphones')
+        return [true_count, skipped_files]
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import zipfile
 import Model
 from flask import Flask
 from distutils.log import debug
@@ -5,11 +7,11 @@ from fileinput import filename
 from flask import *
 import PredictionService
 
-
 app = Flask(__name__)
+predictionService: PredictionService = None
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def main():
     return render_template("index.html")
 
@@ -18,23 +20,34 @@ def main():
 def success():
     if request.method == 'POST':
         f = request.files['file']
-
-        if '.wav' in f.filename:
-
+        prediction_service = PredictionService.PredictionService()
+        if f.filename.endswith('.wav'):
             f.save(f.filename)
-
-            prediction_service = PredictionService.Prediction()
             prediction = prediction_service.predict_audio(f.filename)
 
-            #print(prediction)
+            # print(prediction)
 
             result = 'contains' if prediction == 1 else 'does not contain'
-            #print(f.filename)
+            # print(f.filename)
 
             return render_template("file_upload.html", result=result, file_path=f.filename)
+        elif '.zip' in f.filename:
+            temp_dir_path = 'temp'
+            os.makedirs(temp_dir_path, exist_ok=True)
+            file_path = os.path.join(temp_dir_path, f.filename).replace('\\', '/')
+            f.save(file_path)
+            with zipfile.ZipFile(file_path, 'r') as zip_ref:
+                zip_ref.extractall(temp_dir_path)
+            os.remove(file_path)
+            result, skipped_files = prediction_service.predict_territory(
+                temp_dir_path + f'/{os.path.splitext(os.path.basename(f.filename))[0]}/')
+            return render_template("zip_identification.html", result=result, file_path=f.filename,
+                                   skipped_files=skipped_files)
         else:
             return render_template("wrong_file.html")
 
 
 if __name__ == '__main__':
     app.run()
+    if predictionService is None:
+        predictionService = PredictionService.PredictionService()
