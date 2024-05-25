c05bcb7a6e3d535192a00f08862994d7bbb48122
YuriyGonsor <yurii.gonsor@logivations.com>
2024-05-25 11:47:58 +0300

    minor fix

--- services/ModelService.py
+++ services/ModelService.py
@@ -12,9 +12,7 @@ class ModelService:
         loaded_model_json = json_file.read()
         json_file.close()
         loaded_model = model_from_json(loaded_model_json)
-        # load weights into new model
         loaded_model.load_weights(f"cnn_models/{model_name}.h5")
-        # print("Loaded model from disk")
         loaded_model.compile(loss='binary_crossentropy', optimizer='Adam', metrics=['accuracy'])
         self.loaded_model = loaded_model
 
